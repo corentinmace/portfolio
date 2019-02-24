27777d4b2bb6c90d155e81ce38f30dbe7a3ff3bc
corentinmace <corentin.mace@ynov.com>
2019-02-24 02:26:41 +0100

    commiting for github.io site

--- assets/php/head.php
+++ assets/php/head.php
@@ -8,6 +8,7 @@
 <link rel="stylesheet" href="assets/css/header.css">
 <link rel="stylesheet" href="assets/css/main.css">
 <link rel="stylesheet" href="assets/css/sideInfo.css">
+<link rel="shortcut icon" href="assets/img/favicon.png">
 <script src="assets/js/smooth.js"></script>
 <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 <script src="assets/js/sideInfo.js"></script>
