db72ff98f04aaa7a9d3f9c9a80b938631037bfca
corentinmace <corentin.mace@ynov.com>
2019-06-14 18:03:56 +0200

    blabla

--- FR/assets/php/admin_page.php
+++ FR/assets/php/admin_page.php
@@ -17,20 +17,41 @@
           exit;
       }
        ?>
-
-       <div class="container-admin">
-         <div class="comp-inner">
-           <h3>Ajouter une compétence</h3>
-           <form action="ajout_comp.php" method="POST">
-               <input name="comp" type="text" placeholder="Compétence">
-               <input name="percent" type="number" min="25" max="100" step="5" placeholder="50%">
-               <button id="sendButton" type="submit">Ajouter</button>
-           </form>
-         </div>
-         <div class="comp-inner">
-           <h3>Modifier une compétence</h3>
-           <form action="modif_comp.php" method="POST">
-               <select name="comp">
+       <main>
+         <button onclick="window.location.href='../../index.php'" id="sendButton" type="button" name="button">Retour au site</button>
+         <div class="container-admin">
+           <div class="comp-inner">
+             <h3>Ajouter une compétence</h3>
+             <form action="ajout_comp.php" method="POST">
+                 <input name="comp" type="text" placeholder="Compétence">
+                 <input name="percent" type="number" min="25" max="100" step="5" placeholder="50%">
+                 <button id="sendButton" type="submit">Ajouter</button>
+             </form>
+           </div>
+           <div class="comp-inner">
+             <h3>Modifier une compétence</h3>
+             <form action="modif_comp.php" method="POST">
+                 <select name="comp">
+                   <?php
+                       $tab = "SELECT nom_comp FROM competences;";
+                       $sql = $dbh->prepare($tab);
+                       $sql->execute();
+                       $tabulation = [];
+                       while($ligne = $sql->fetch(PDO::FETCH_NUM)){
+                           foreach($ligne as $val){
+                               echo "<option name='$val'>$val</option>";
+                           }
+                       }
+                       ?>
+                   </select>
+                 <input name="percent" type="number" min="50" max="100" step="5" placeholder="50%">
+                 <button id="sendButton" type="submit">Modifier</button>
+             </form>
+           </div>
+           <div class="comp-inner">
+             <h3>Supprimer une compétence</h3>
+             <form action="deletecomp.php" method="POST">
+                 <select name="comp">
                  <?php
                      $tab = "SELECT nom_comp FROM competences;";
                      $sql = $dbh->prepare($tab);
@@ -43,31 +64,11 @@
                      }
                      ?>
                  </select>
-               <input name="percent" type="number" min="50" max="100" step="5" placeholder="50%">
-               <button id="sendButton" type="submit">Modifier</button>
-           </form>
+                 <button id="sendButton" type="submit">Supprimer</button>
+             </form>
+           </div>
          </div>
-         <div class="comp-inner">
-           <h3>Supprimer une compétence</h3>
-           <form action="deletecomp.php" method="POST">
-               <select name="comp">
-               <?php
-                   $tab = "SELECT nom_comp FROM competences;";
-                   $sql = $dbh->prepare($tab);
-                   $sql->execute();
-                   $tabulation = [];
-                   while($ligne = $sql->fetch(PDO::FETCH_NUM)){
-                       foreach($ligne as $val){
-                           echo "<option name='$val'>$val</option>";
-                       }
-                   }
-                   ?>
-               </select>
-               <button id="sendButton" type="submit">Supprimer</button>
-           </form>
-         </div>
-       </div>
-
+      </main>
 
   </body>
 </html>
